4139725b2520623e11f4d8ee60d1dfd553137e68
lc-codes496 <lucascavalnte2@gmail.com>
2025-10-05 17:37:52 -0300

    Update game.java

--- src/game.java
+++ src/game.java
@@ -1,15 +1,9 @@
 package src;
 
-import org.lwjgl.glfw.GLFW;
-import org.lwjgl.opengl.GL11;
-
 public class Game {
-    private World world;
-    private Player player;
+    public Player player;
+    public World world;
     private Renderer renderer;
-
-    private boolean running = true;
-    private double lastTime;
 
     public Game() {
         world = new World();
@@ -17,31 +11,9 @@ public class Game {
         renderer = new Renderer();
     }
 
-    public void update() {
-        double currentTime = GLFW.glfwGetTime();
-        double deltaTime = currentTime - lastTime;
-        lastTime = currentTime;
-
-        // Atualiza jogador (movimento, física, etc)
+    public void update(double deltaTime) {
         player.update(deltaTime);
-
-        // Atualiza mundo (se precisar)
         world.update();
-
-        // Renderiza tudo
-        render();
-    }
-
-    private void render() {
-        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
         renderer.renderWorld(world, player);
-    }
-
-    public void stop() {
-        running = false;
-    }
-
-    public boolean isRunning() {
-        return running;
     }
 }
