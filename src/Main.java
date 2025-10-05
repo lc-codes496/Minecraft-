3b9eb00c76b9879cd1b8e5c33cd5e0ee43915db8
lc-codes496 <lucascavalnte2@gmail.com>
2025-10-05 17:33:47 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ package src;
 
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL;
-import org.lwjgl.opengl.GL11;
 
 public class Main {
     private long window;
@@ -13,48 +12,33 @@ public class Main {
     private Input input;
 
     public void run() {
-        System.out.println("Iniciando Minecraft Java!");
-        init();
-        loop();
-
-        GLFW.glfwDestroyWindow(window);
-        GLFW.glfwTerminate();
-    }
-
-    private void init() {
-        if (!GLFW.glfwInit()) {
-            throw new IllegalStateException("Falha ao iniciar GLFW!");
-        }
-
+        if (!GLFW.glfwInit()) throw new IllegalStateException("Falha ao iniciar GLFW!");
         window = GLFW.glfwCreateWindow(width, height, "Minecraft Java", 0, 0);
         if (window == 0) throw new RuntimeException("Falha ao criar janela!");
-
         GLFW.glfwMakeContextCurrent(window);
         GLFW.glfwShowWindow(window);
         GL.createCapabilities();
 
         GL11.glEnable(GL11.GL_DEPTH_TEST);
 
-        // Inicializa Input
         input = new Input(window);
-
-        // Inicializa o Game e conecta o Input ao Player
         game = new Game();
         game.player.setInput(input);
+
+        loop();
+
+        GLFW.glfwDestroyWindow(window);
+        GLFW.glfwTerminate();
     }
 
     private void loop() {
         double lastTime = GLFW.glfwGetTime();
-
         while (!GLFW.glfwWindowShouldClose(window)) {
             double currentTime = GLFW.glfwGetTime();
             double deltaTime = currentTime - lastTime;
             lastTime = currentTime;
 
-            // Atualiza o jogo
-            game.update();
-
-            // Troca buffers
+            game.update(deltaTime);
             GLFW.glfwSwapBuffers(window);
             GLFW.glfwPollEvents();
         }
