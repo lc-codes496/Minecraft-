07934bafbc1770ff35482404a6f2e777e7b1f4f4
lc-codes496 <lucascavalnte2@gmail.com>
2025-10-05 17:25:07 -0300

    Update Renderer.java

--- src/Renderer.java
+++ src/Renderer.java
@@ -1,22 +1,19 @@
 package src;
 
-import org.lwjgl.opengl.GL11;
-import org.joml.Matrix4f;
 import org.joml.Vector3f;
+import org.lwjgl.opengl.GL11;
 
 public class Renderer {
 
     public Renderer() {
-        // Configurações básicas de OpenGL
         GL11.glEnable(GL11.GL_DEPTH_TEST);
-        GL11.glClearColor(0.3f, 0.6f, 1.0f, 0.0f); // céu azul
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glClearColor(0.3f, 0.6f, 1.0f, 0f); // céu azul
     }
 
     public void renderWorld(World world, Player player) {
-        // Aqui podemos adicionar a projeção e view da câmera
         setupCamera(player);
 
-        // Renderizar blocos do mundo
         for (Block block : world.getBlocks()) {
             renderBlock(block);
         }
@@ -25,16 +22,12 @@ public class Renderer {
     private void setupCamera(Player player) {
         GL11.glMatrixMode(GL11.GL_PROJECTION);
         GL11.glLoadIdentity();
-        // Perspectiva básica
         float aspect = 800f / 600f;
         float fov = 70f;
         float near = 0.1f;
         float far = 100f;
         float y_scale = (float) (1f / Math.tan(Math.toRadians(fov / 2f)));
         float x_scale = y_scale / aspect;
-        float frustum_length = far - near;
-
-        // Usando matriz OpenGL legacy para simplificar
         GL11.glFrustum(-x_scale, x_scale, -y_scale, y_scale, near, far);
 
         GL11.glMatrixMode(GL11.GL_MODELVIEW);
@@ -44,37 +37,36 @@ public class Renderer {
         GL11.glTranslatef(-player.getPosition().x, -player.getPosition().y, -player.getPosition().z);
     }
 
-    private void renderBlock(Block block) {
+    public void renderBlock(Block block) {
         Vector3f pos = block.getPosition();
+        int texID = block.getTextureID();
 
         GL11.glPushMatrix();
         GL11.glTranslatef(pos.x, pos.y, pos.z);
 
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, texID);
+
         GL11.glBegin(GL11.GL_QUADS);
 
-        // Cubo simples com cores diferentes para cada face
         // Frente
-        GL11.glColor3f(1, 0, 0); // vermelho
-        GL11.glVertex3f(0, 0, 0);
-        GL11.glVertex3f(1, 0, 0);
-        GL11.glVertex3f(1, 1, 0);
-        GL11.glVertex3f(0, 1, 0);
+        GL11.glTexCoord2f(0, 0); GL11.glVertex3f(0, 0, 0);
+        GL11.glTexCoord2f(1, 0); GL11.glVertex3f(1, 0, 0);
+        GL11.glTexCoord2f(1, 1); GL11.glVertex3f(1, 1, 0);
+        GL11.glTexCoord2f(0, 1); GL11.glVertex3f(0, 1, 0);
 
         // Trás
-        GL11.glColor3f(0, 1, 0); // verde
-        GL11.glVertex3f(0, 0, 1);
-        GL11.glVertex3f(1, 0, 1);
-        GL11.glVertex3f(1, 1, 1);
-        GL11.glVertex3f(0, 1, 1);
+        GL11.glTexCoord2f(0, 0); GL11.glVertex3f(0, 0, 1);
+        GL11.glTexCoord2f(1, 0); GL11.glVertex3f(1, 0, 1);
+        GL11.glTexCoord2f(1, 1); GL11.glVertex3f(1, 1, 1);
+        GL11.glTexCoord2f(0, 1); GL11.glVertex3f(0, 1, 1);
 
         // Lados
-        GL11.glColor3f(0, 0, 1); // azul
-        GL11.glVertex3f(0,0,0); GL11.glVertex3f(0,1,0); GL11.glVertex3f(0,1,1); GL11.glVertex3f(0,0,1); // esquerda
-        GL11.glVertex3f(1,0,0); GL11.glVertex3f(1,1,0); GL11.glVertex3f(1,1,1); GL11.glVertex3f(1,0,1); // direita
-        GL11.glVertex3f(0,1,0); GL11.glVertex3f(1,1,0); GL11.glVertex3f(1,1,1); GL11.glVertex3f(0,1,1); // topo
-        GL11.glVertex3f(0,0,0); GL11.glVertex3f(1,0,0); GL11.glVertex3f(1,0,1); GL11.glVertex3f(0,0,1); // base
+        GL11.glTexCoord2f(0,0); GL11.glVertex3f(0,0,0); GL11.glTexCoord2f(1,0); GL11.glVertex3f(0,1,0); GL11.glTexCoord2f(1,1); GL11.glVertex3f(0,1,1); GL11.glTexCoord2f(0,1); GL11.glVertex3f(0,0,1);
+        GL11.glTexCoord2f(0,0); GL11.glVertex3f(1,0,0); GL11.glTexCoord2f(1,0); GL11.glVertex3f(1,1,0); GL11.glTexCoord2f(1,1); GL11.glVertex3f(1,1,1); GL11.glTexCoord2f(0,1); GL11.glVertex3f(1,0,1);
+        GL11.glTexCoord2f(0,0); GL11.glVertex3f(0,1,0); GL11.glTexCoord2f(1,0); GL11.glVertex3f(1,1,0); GL11.glTexCoord2f(1,1); GL11.glVertex3f(1,1,1); GL11.glTexCoord2f(0,1); GL11.glVertex3f(0,1,1);
+        GL11.glTexCoord2f(0,0); GL11.glVertex3f(0,0,0); GL11.glTexCoord2f(1,0); GL11.glVertex3f(1,0,0); GL11.glTexCoord2f(1,1); GL11.glVertex3f(1,0,1); GL11.glTexCoord2f(0,1); GL11.glVertex3f(0,0,1);
 
         GL11.glEnd();
         GL11.glPopMatrix();
     }
-                       }
+            }
