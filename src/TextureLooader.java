5605bdbee9db66fd2112ebdeb88f71799d311613
lc-codes496 <lucascavalnte2@gmail.com>
2025-10-05 17:20:29 -0300

    Create TextureLooader.java

---
+++ src/TextureLooader.java
@@ -0,0 +1,34 @@
+package src;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.stb.STBImage;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+public class TextureLoader {
+
+    public static int loadTexture(String path) {
+        IntBuffer width = BufferUtils.createIntBuffer(1);
+        IntBuffer height = BufferUtils.createIntBuffer(1);
+        IntBuffer channels = BufferUtils.createIntBuffer(1);
+
+        ByteBuffer image = STBImage.stbi_load(path, width, height, channels, 4);
+        if (image == null) {
+            throw new RuntimeException("Falha ao carregar textura: " + path);
+        }
+
+        int textureID = GL11.glGenTextures();
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureID);
+
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
+
+        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, width.get(), height.get(), 0,
+                GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, image);
+
+        STBImage.stbi_image_free(image);
+        return textureID;
+    }
+}
